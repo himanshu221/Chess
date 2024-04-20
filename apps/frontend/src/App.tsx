7c76f96d537dc421aa1e59ece6a35a0dad69c54d
Himanshu <himanshubhushan775@gmail.com>
2024-04-20 14:53:47 +0530

    Added start button in the UI

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Landing } from './screens/Landing'
 import { Game } from './screens/Game'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
       <Routes>
--- apps/frontend/src/screens/Game.tsx
+++ apps/frontend/src/screens/Game.tsx
@@ -12,25 +12,26 @@ export const Game = () => {
             <div className="col-span-1 md:col-span-2 flex justify-center items-center">
                 <Chessboard position={'start'} boardWidth={580} customBoardStyle={{borderRadius: '5px', boxShadow: '0 0 50px rgba(0, 0, 0, 0.9'}} />
             </div>
-            <div className="col-span-1 md:col-span-1 overflow-y-scroll border-white border rounded-md min-h-20">
-                <table className="table-fixed w-[100%]">
-                    <thead className="bg-slate-200 w-[100%] top-0 sticky">
-                        <tr>
-                            <th>From</th>
-                            <th>To</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {startGame ? null : 
-                            <div className="flex justify-center items-center">
-                                <button onClick={handleClick} type="button" className="text-white bg-[#FF9119] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-3xl px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 me-2 mb-2">
-                                    Start
-                                </button>
-                            </div>
-                         }
-                    </tbody>
-                </table>
-            </div>
+                {startGame ? 
+                    <div className="col-span-1 md:col-span-1 overflow-y-scroll border-white border flex justify-center rounded-md min-h-20">
+                        <table className="table-fixed w-[100%]">
+                            <thead className="bg-slate-200 w-[100%] top-0 sticky">
+                                <tr>
+                                    <th>From</th>
+                                    <th>To</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                    :
+                    <div className="flex justify-center items-center">
+                        <button onClick={handleClick} type="button" className="text-white bg-[#FF9119] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-3xl px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 me-2 mb-2">
+                            Start Game
+                        </button>
+                    </div>
+                }
         </div>
     </div>
 }
