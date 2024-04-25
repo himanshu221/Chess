8f86a81d778f3b83e6243df9251824523b3fd0f5
Himanshu <himanshubhushan775@gmail.com>
2024-04-26 01:03:37 +0530

    added count in move table

--- apps/frontend/src/components/MoveTable.tsx
+++ apps/frontend/src/components/MoveTable.tsx
@@ -1,14 +1,23 @@
 export const MoveTable = ({moves} : {moves: string[]}) => {
-    return <div className="col-span-1 md:col-span-1 overflow-y-scroll border-slate-300 border rounded-md min-h-20">
-        <div className="bg-slate-300 h-12 w-[100%] top-0 col-span-2 flex justify-center items-center sticky font-bold text-xl">
+    let count = 1;
+    return <div className="col-span-1 md:col-span-1 overflow-y-scroll backdrop-blur-none border-[#25150c] border-4 rounded-md min-h-20">
+        <div className="bg-[#25150c] h-12 w-[100%] top-0 col-span-2 flex justify-center items-center sticky font-bold text-xl text-white">
             Moves
         </div>
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-7">
             {
                 moves.map((move, ind) => 
-                    <div key={ind} className="col-span-1 text-lg text-white flex justify-center items-center h-12">
-                        {move}
-                    </div>)
+                    <>
+                        {
+                            ind%2 == 0 ? <div key={count} className="col-span-1 text-lg text-slate-300 flex justify-center items-center h-12">
+                                {`${count++} .`}
+                            </div> : null
+
+                        }
+                        <div key={ind} className="col-span-3 text-lg text-white flex justify-center items-center h-12">
+                            {move}
+                        </div>
+                    </>)
             }
         </div>
     </div>
